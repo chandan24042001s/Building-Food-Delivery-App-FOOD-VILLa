06fd0907e982764de3cc640130f6c28b4d48a344
Kuldeep Saini <142159571+kuldeep-saini-5514@users.noreply.github.com>
2023-12-13 00:56:35 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      Orange: "#d97919",
+      LightOrange: "#c26100",
+      Green: "#016034",
+      LightGreen: "#00ad1d",
+      Primary: "#545454",
+      Secondry: "#eaeaea",
+      White: "#ffffff",
+      Blue: "#4b6cb7",
+      DarkOrange: "#ffaf60",
+      Gray: "#2f2f2f",
+      DarkGray: "#dadada",
+    },
+    fontFamily: {
+      PT: ["PT Sans"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};

e984785230f1523f66436e9a2a71a241cac24015
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2023-12-21 13:37:49 +0530

    bug fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    colors: {
-      Orange: "#d97919",
-      LightOrange: "#c26100",
-      Green: "#016034",
-      LightGreen: "#00ad1d",
-      Primary: "#545454",
-      Secondry: "#eaeaea",
-      White: "#ffffff",
-      Blue: "#4b6cb7",
-      DarkOrange: "#ffaf60",
-      Gray: "#2f2f2f",
-      DarkGray: "#dadada",
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        Orange: "#d97919",
+        LightOrange: "#c26100",
+        Green: "#016034",
+        LightGreen: "#00ad1d",
+        Primary: "#545454",
+        Secondry: "#eaeaea",
+        White: "#ffffff",
+        Blue: "#4b6cb7",
+        DarkOrange: "#ffaf60",
+        Gray: "#2f2f2f",
+        DarkGray: "#dadada",
+        BlackGray: "#545454",
+      },
+      fontFamily: {
+        PT: ["PT Sans"],
+      },
     },
-    fontFamily: {
-      PT: ["PT Sans"],
-    },
-    extend: {},
   },
   plugins: [],
-};
+}

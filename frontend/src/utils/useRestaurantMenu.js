4c6ea12e27e530e4e3de1088c97e63983694b95f
Chandan <hb7102849@gmail.com>
2024-02-20 01:06:14 +0530

    menu

--- frontend/src/utils/useRestaurantMenu.js
+++ frontend/src/utils/useRestaurantMenu.js
@@ -8,8 +8,8 @@ const useRestaurantMenu = (resId) => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      `/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5987633&lng=77.0786143&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+    const data = await fetch("https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId="+resId
+      // `/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5987633&lng=77.0786143&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
     );
 
     const json = await data.json();
